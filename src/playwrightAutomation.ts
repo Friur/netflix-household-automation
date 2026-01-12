53a1fa480b7d6a2246ea29ed6a1e3b3441759b7b
Pedro Henrique <pedrowqi@gmail.com>
2026-01-12 20:52:34 -0300

    DESFAZER COMMIT

--- src/playwrightAutomation.ts
+++ src/playwrightAutomation.ts
@@ -1,22 +1,13 @@
-import { existsSync } from "fs";
 import { chromium, expect } from '@playwright/test';
 import Errorlogger from './Errorlogger';
 
-const STORAGE_STATE_PATH = './tmp/storageState.json';
-
 export default async function playwrightAutomation(url: string) {
-  const storageStateExists = existsSync(STORAGE_STATE_PATH);
-
   const browser = await chromium.launch({
     headless: true,
     args: ['--disable-gl-drawing-for-tests'], // disable GPU drawing for improved performance in headless mode.
   });
 
-  // Load the storage state start with the previous Session/Cookie.
-  // This prevents Netflix from sending emails about new devices using the account.
-  const browserContext = await browser.newContext({
-    storageState: storageStateExists ? STORAGE_STATE_PATH : undefined
-  });
+  const browserContext = await browser.newContext();
   const page = await browserContext.newPage();
 
   try {
@@ -31,8 +22,6 @@ export default async function playwrightAutomation(url: string) {
       intervals: [100, 250, 500, 1_000],
       timeout: 30_000,
     });
-
-    await browserContext.storageState({ path: STORAGE_STATE_PATH });
   } catch (error) {
     throw new Errorlogger(`No Netflix location update button found for URL, maybe link timeout already expired: ${error}`);
   } finally {
